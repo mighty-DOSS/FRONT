aafb95f4d4ad07a3a5877b067e1dc60de0901bbe
mighty-DOS <h.webprojects@outlook.com>
2021-09-28 21:15:27 -0300

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: [],
 })
-export class AppComponent implements OnInit{
-  title!: string;
-  ngOnInit():void {
-    console.log("Evento executado na inicialização do componente !");
-  }
-}
+export class AppComponent{}
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,3 +1,8 @@
+import { ListarQuartoComponent } from './components/views/quarto/listar-quarto/listar-quarto.component';
+import { ListarReservaComponent } from './components/views/reserva/listar-reserva/listar-reserva.component';
+import { CadastrarReservaComponent } from './components/views/reserva/cadastrar-reserva/cadastrar-reserva.component';
+import { ListarClienteComponent } from './components/views/cliente/listar-cliente/listar-cliente.component';
+import { CadastrarClienteComponent } from './components/views/cliente/cadastrar-cliente/cadastrar-cliente.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarProdutoComponent } from "./components/views/produto/cadastrar-produto/cadastrar-produto.component";
@@ -15,6 +20,30 @@ const routes: Routes = [
     {
         path: "produto/cadastrar",
         component: CadastrarProdutoComponent,
+    },
+    {
+        path: "cliente/cadastrar",
+        component: CadastrarClienteComponent,
+    },
+    {
+        path: "cliente/listar",
+        component: ListarClienteComponent,
+    },
+    {
+        path: "reserva/cadastrar",
+        component: CadastrarReservaComponent,
+    },
+    {
+        path: "reserva/listar",
+        component: ListarReservaComponent,
+    },
+    {
+        path: "quarto/cadastrar",
+        component: CadastrarReservaComponent,
+    },
+    {
+        path: "quarto/listar",
+        component: ListarQuartoComponent
     },
 ];
 
