b6c0a1ae372df8b638e4e7f7c0cb46ee79da38a2
mighty-DOS <h.webprojects@outlook.com>
2021-10-04 14:36:11 -0300

    master

--- src/app/components/views/reserva/cadastrar-reserva/cadastrar-reserva.component.ts
+++ src/app/components/views/reserva/cadastrar-reserva/cadastrar-reserva.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReservaService } from 'src/app/services/reserva.service';
+import { Reserva } from 'src/app/models/reserva';
 
 @Component({
   selector: 'app-cadastrar-reserva',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar-reserva.component.scss']
 })
 export class CadastrarReservaComponent implements OnInit {
+    cliente!: string;
+    quarto!: number;
+    datacheckin!: number;
+    datacheckout!: number;
+    valortotal!: number;
+    criadoem!: number;
 
-  constructor() { }
+  constructor(private service: ReservaService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  cadastrarreserva(): void{
+      let reserva: Reserva = {
+        Cliente: this.cliente,
+        Quarto: this.quarto,
+        DataCheckin: this.datacheckin,
+        DataCheckout: this.datacheckout,
+        ValorTotal: this.valortotal,
+        CriadoEm: this.criadoem,
+        
+      };
+      this.service.create(reserva).subscribe((reserva) =>{
+        console.log(reserva);
+        this.router.navigate([""]);
+      });
   }
 
 }
+
