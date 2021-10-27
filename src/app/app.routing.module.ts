6f7629cbaf5c5389743625b7a3c50bfde4275abe
mighty-DOS <h.webprojects@outlook.com>
2021-10-27 09:44:19 -0300

    master

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,11 +7,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarProdutoComponent } from "./components/views/produto/cadastrar-produto/cadastrar-produto.component";
 import { ListarProdutoComponent } from "./components/views/produto/listar-produto/listar-produto.component";
+import { ComponentSidebarComponent } from './components/component-sidebar/component-sidebar.component';
 
 const routes: Routes = [
     {
         path: "",
-        component: ListarProdutoComponent,
+        component: ComponentSidebarComponent,
     },
     {
         path: "produto/listar",
